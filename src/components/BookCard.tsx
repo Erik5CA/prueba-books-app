0c6649095b4502ed32eab9eeb754aef0ddc84119
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-05 17:47:47 -0600

    feature: add book to reading list

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,12 +1,20 @@
+import { useAppDispatch } from "../hooks/books";
+import { addToReadingList } from "../slices/bookSlice";
 import { Book } from "../types";
 
 const BookCard = ({ book }: { book: Book }) => {
+  const dispatch = useAppDispatch();
   return (
     <div className="book-card">
       <img className="book-card__image" src={book.cover} alt={book.title} />
       <div className="book-card__wrapper">
         <h2 className="book-card__title">{book.title}</h2>
-        <button className="book-card__btn">Add To Lecture</button>
+        <button
+          onClick={() => dispatch(addToReadingList(book))}
+          className="book-card__btn"
+        >
+          Add To Lecture
+        </button>
       </div>
     </div>
   );
