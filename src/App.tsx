8006eef4d2281610666cacc7d8eef983cad5959a
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-05 17:45:55 -0600

    add: dependency react hot toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import BookContainer from "./components/BookContainer";
 import FiltersContainer from "./components/FiltersContainer";
 function App() {
   return (
     <>
+      <Toaster />
       <header>
         <h1>Books APP</h1>
       </header>
