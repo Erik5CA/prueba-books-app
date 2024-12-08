b2cedb3e0e25c32b671a0d3a1031f251ee3dabc1
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-08 17:27:53 -0600

    add: books avelables, books by genre

--- src/components/BookContainer.tsx
+++ src/components/BookContainer.tsx
@@ -2,12 +2,30 @@ import { useAppSelector } from "../hooks/books";
 import BookCard from "./BookCard";
 
 const BookContainer = () => {
-  const books = useAppSelector((state) => state.book.library);
+  const {
+    library: books,
+    filter,
+    booksAvelable,
+  } = useAppSelector((state) => state.book);
   return (
-    <div className="books__container">
-      {books.map(({ book }) => (
-        <BookCard book={book} key={book.title} />
-      ))}
+    <div className="books">
+      <div className="books__header">
+        <h3 className="books__title">
+          <span className="books__number">{booksAvelable}</span> disponibles
+        </h3>
+        {filter !== "Todos" && (
+          <h3 className="books__title">
+            <span className="books__number">{books.length}</span> libros
+            encontrados para el genero{" "}
+            <span className="books__genre">{filter}</span>
+          </h3>
+        )}
+      </div>
+      <div className="books__container">
+        {books.map(({ book }) => (
+          <BookCard book={book} key={book.title} />
+        ))}
+      </div>
     </div>
   );
 };
