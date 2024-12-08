7a6ed383fd2cdeb04ddf7be429d8bf49150e8877
Erik5CA <erik.castillo.anzures@gmail.com>
2024-12-08 17:26:51 -0600

    add state to check if a book is in a readinf list, show more book info

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,20 +1,37 @@
-import { useAppDispatch } from "../hooks/books";
-import { addToReadingList } from "../slices/bookSlice";
+import { useAppDispatch, useAppSelector } from "../hooks/books";
+import { addToReadingList, removeFromReadingList } from "../slices/bookSlice";
 import { Book } from "../types";
+import { BookCheck } from "lucide-react";
 
 const BookCard = ({ book }: { book: Book }) => {
+  const booksAdded = useAppSelector((state) => state.book.redingListIDs);
   const dispatch = useAppDispatch();
+  const isAdded = booksAdded.includes(book.ISBN);
   return (
     <div className="book-card">
+      <div className="book-card__added">{isAdded && <BookCheck />}</div>
       <img className="book-card__image" src={book.cover} alt={book.title} />
       <div className="book-card__wrapper">
-        <h2 className="book-card__title">{book.title}</h2>
-        <button
-          onClick={() => dispatch(addToReadingList(book))}
-          className="book-card__btn"
-        >
-          Add To Lecture
-        </button>
+        <div className="book-card__info">
+          <h2 className="book-card__title">{book.title}</h2>
+          <p>{book.author.name}</p>
+          <p>{book.year}</p>
+        </div>
+        {isAdded ? (
+          <button
+            onClick={() => dispatch(removeFromReadingList(book.ISBN))}
+            className="book-card__btn book-card__btn--danger"
+          >
+            Remover
+          </button>
+        ) : (
+          <button
+            onClick={() => dispatch(addToReadingList(book))}
+            className="book-card__btn"
+          >
+            Añadir
+          </button>
+        )}
       </div>
     </div>
   );
